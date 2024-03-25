31ce498e32d3ec199e0f85cd66b626ab74fea170
ikufumi Mitsuyu <ikufumimitsuyu@icloud.com>
2024-03-25 16:52:20 +1000

    import js file into html file

--- Page1.js
+++ Page1.js
@@ -1,14 +1,14 @@
-// Store the frequently used div tags 
-const config = {
+ // Store the frequently used div tags 
+ const config = {
     initialForm: document.getElementById("initial-form"),
     bankPage: document.getElementById("bankPage"),
 }
 
-// Difine a class 
+// Define a class 
 class BankAccount {
 
-    // constructor to initialise a object 
-    constructor(firstName, lastName, email, type, accountNumber,money) {
+    // constructor to initialize an object 
+    constructor(firstName, lastName, email, type, accountNumber, money) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -23,24 +23,25 @@ class BankAccount {
     }
 }
 
-// Generate a random interger between min and max
+// Generate a random integer between min and max
 function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-// called when the users push the submit button
+let user; // Define user in the global scope
+
+// Called when the user pushes the submit button
 function initialiseUserAccount() {
     const form = document.getElementById("bank-form");
 
-    let user = new BankAccount(
+    user = new BankAccount(
         form.querySelectorAll(`input[name="userFirstName"]`)[0].value,
         form.querySelectorAll(`input[name="userLastName"]`)[0].value,
         form.querySelectorAll(`input[name="userEmail"]`)[0].value,
-        form.querySelectorAll(`input[name="userAccountType"]: checked`)[0].value,
-        getRandomInteger(0, Math.pow(10,8)),
+        form.querySelectorAll(`input[name="userAccountType"]:checked`)[0].value,
+        getRandomInteger(0, Math.pow(10, 8)),
         parseInt(form.querySelectorAll(`input[name="userFirstDeposit"]`)[0].value)
-    )
+    );
 
     console.log(user);
 }
-
